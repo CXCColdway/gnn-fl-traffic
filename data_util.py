fc5f1e795477ae2582947fd88c503b52a046b634
CXCColdway <137020302+CXCColdway@users.noreply.github.com>
2023-07-24 09:33:58 +0800

    data_util.py

--- data_util.py
+++ data_util.py
@@ -1,78 +1,316 @@
-import numpy as np
-import pandas as pd
-from sensors2graph import *
-from sklearn.preprocessing import StandardScaler
-from itertools import islice
-import scipy.sparse as sp
-import dgl
 import torch
+import numpy as np
+import scipy.sparse as sp
+from torchvision import datasets
+from collections import namedtuple
+from torchvision import datasets, transforms
+import pickle as pk
+
+def load_image(args):
+    data_dir = "./data/" + str(args.dataset)
+
+    data_mean = (0.4914, 0.4822, 0.4465)  # equals np.mean(train_set.train_data, axis=(0,1,2))/255
+    data_std = (0.2471, 0.2435, 0.2616)  # equals np.std(train_set.train_data, axis=(0,1,2))/255
+
+    trans = [transforms.RandomCrop(32, padding=4),
+             transforms.RandomHorizontalFlip(0.1),
+             transforms.RandomVerticalFlip(0.1),
+             transforms.ToTensor(),
+             transforms.Normalize(data_mean, data_std)]
+
+    apply_transform = transforms.Compose(trans)
+
+    train_set = datasets.CIFAR10(data_dir, train=True, download=True, transform=apply_transform)
+    test_set = datasets.CIFAR10(data_dir, train=False, download=True, transform=apply_transform)
+
+    train_set.topk = 5
+    train_set.targets = np.array(train_set.targets)
+    test_set.targets = np.array(test_set.targets)
 
 
-def load_data(args):
-    with open(args.sensorsfilepath) as f:
-        sensor_ids = f.read().strip().split(",")
+    # split
+    train_user_groups, test_user_groups, A = split_equal_noniid(
+        train_set, test_set, args.shards, args.edge_frac, args.clients)
 
-    '''读取距离csv文件中的内容，并以字符串的形式存储为一个Pandas DataFrame对象'''
-    distance_df = pd.read_csv(args.disfilepath, dtype={"from": "str", "to": "str"})
 
-    adj_mx = get_adjacency_matrix(distance_df, sensor_ids)  # 调用sensors2graph.py文件生成毗连矩阵
-    sp_mx = sp.coo_matrix(adj_mx)  # 毗连矩阵稀疏化
-    G = dgl.from_scipy(sp_mx)  # 将毗连矩阵转化为图
+def load_cifar10(args):
+    data_dir = "./data/" + str(args.dataset)  # 数据集存储地址
 
-    # 读取metr-la.h5文件内容，并获取数据集中的样本数量和节点数量
-    df = pd.read_hdf(args.tsfilepath)
-    num_samples, num_nodes = df.shape
+    train_set = datasets.CIFAR10(root=data_dir, train=True, download=True)
+    test_set = datasets.CIFAR10(root=data_dir, train=False, download=True)
+    data_mean = (0.4914, 0.4822, 0.4465)  # equals np.mean(train_set.train_data, axis=(0,1,2))/255
+    data_std = (0.2471, 0.2435, 0.2616)  # equals np.std(train_set.train_data, axis=(0,1,2))/255
 
-    n_his = args.window
+    train_set.topk = 5
+    train_set.targets = np.array(train_set.targets)
+    test_set.targets = np.array(test_set.targets)
 
-    n_pred = args.pred_len
-    n_route = num_nodes
-
-    batch_size = args.gnn_batch_size
+    train_transforms = [Crop(32, 32), FlipLR(), Cutout(8, 8)]  # 数据扩增操作
 
     # 划分数据集
-    len_val = round(num_samples * 0.1)
-    len_train = round(num_samples * 0.7)
-    train = df[:len_train]
-    val = df[len_train: len_train + len_val]
-    test = df[len_train + len_val:]
+    train_user_groups, test_user_groups, A = split_equal_noniid(
+        train_set, test_set, args.shards, args.edge_frac, args.clients)
 
-    # 标准化数据集
-    scaler = StandardScaler()
-    train = scaler.fit_transform(train)
-    val = scaler.transform(val)
-    test = scaler.transform(test)
+    train_set = list(zip(transpose(normalise(pad(train_set.data, 4), data_mean, data_std)), train_set.targets))# 获取训练集
+    test_set = list(zip(transpose(normalise(test_set.data, data_mean, data_std)), test_set.targets))# 获取测试集
 
-    # 划分数据样本，生成Pytorch的Tensor对象,调用load_data.py文件
-    x_train, y_train = data_transform(train, n_his, n_pred, args.device)
-    x_val, y_val = data_transform(val, n_his, n_pred, args.device)
-    x_test, y_test = data_transform(test, n_his, n_pred, args.device)
+    # 将训练集和测试集转换为各个用户客户端上用于训练的小批量数据
+    train_batches = []
+    test_batches = []
+    for key, users in train_user_groups.items():
+        train_batches.append(Batches(Transform([train_set[u.astype(int)] for u in users],
+                train_transforms), args.batch_size, shuffle=True, device=args.device,
+                set_random_choices=True, drop_last=True))
+    for key, users in test_user_groups.items():
+        test_batches.append(Batches([test_set[u.astype(int)] for u in users],
+                args.batch_size, shuffle=False, device=args.device, drop_last=False))
 
-    # 生成训练集、验证集和测试集。使用迭代器输出小批量数据
-    train_data = torch.utils.data.TensorDataset(x_train, y_train)
-    train_iter = torch.utils.data.DataLoader(train_data, batch_size, shuffle=True)  # len=2385
-    val_data = torch.utils.data.TensorDataset(x_val, y_val)
-    val_iter = torch.utils.data.DataLoader(val_data, batch_size)
-    test_data = torch.utils.data.TensorDataset(x_test, y_test)
-    test_iter = torch.utils.data.DataLoader(test_data, batch_size)  # len=671
+    overall_tbatches = Batches(test_set, args.batch_size, shuffle=False,
+                               device=args.device, drop_last=False)
 
-    return train_iter, test_iter, val_iter, G, n_route, adj_mx
+    return train_batches, test_batches, A, overall_tbatches
 
 
-def data_transform(data, n_his, n_pred, device):
-    # produce data slices for training and testing
-    n_route = data.shape[1] # 获取数据集对应的采样数量
-    l = len(data)
-    num = l - n_his - n_pred
-    x = np.zeros([num, 1, n_his, n_route])
-    y = np.zeros([num, n_route])
+# Image data related
+def load_mnist(args):
 
-    cnt = 0
-    for i in range(l - n_his - n_pred):
-        head = i
-        tail = i + n_his
-        x[cnt, :, :, :] = data[head:tail].reshape(1, n_his, n_route)
-        y[cnt] = data[tail + n_pred - 1]
-        cnt += 1
-    return torch.Tensor(x).to(device), torch.Tensor(y).to(device)
-    
+    data_dir = "./data/" + str(args.dataset)
+
+    trans = [transforms.ToTensor(),
+         transforms.Normalize(*((0.1307,), (0.3081,)))]
+
+    apply_transform = transforms.Compose(trans)
+
+    train_dataset = datasets.MNIST(data_dir, train=True, download=True, transform=apply_transform)
+    test_dataset = datasets.MNIST(data_dir, train=False, download=True, transform=apply_transform)
+
+    train_dataset.topk = 5
+    train_dataset.data = torch.unsqueeze(train_dataset.data, 1)
+    train_dataset.targets = np.array(train_dataset.targets)
+    train_dataset.data = train_dataset.data.type(torch.FloatTensor)
+    test_dataset.data = torch.unsqueeze(test_dataset.data, 1)
+    test_dataset.targets = np.array(test_dataset.targets)
+    test_dataset.data = test_dataset.data.type(torch.FloatTensor)
+
+    train_user_groups, test_user_groups, A = split_equal_noniid(
+        train_dataset, test_dataset, args.shards, args.edge_frac, args.clients)
+
+    train_set = list(zip(train_dataset.data, train_dataset.targets))
+    test_set = list(zip(test_dataset.data, test_dataset.targets))
+
+    train_batches = []
+    test_batches = []
+
+    for key, users in train_user_groups.items():
+        train_batches.append(Batches([train_set[u.astype(int)] for u in users], args.batch_size,
+                                     shuffle=True, device=args.device, drop_last=True))
+
+    for key, users in test_user_groups.items():
+        test_batches.append(Batches([test_set[u.astype(int)] for u in users], args.batch_size,
+                                    shuffle=False, device=args.device, drop_last=False))
+
+    overall_tbatches = Batches(test_set, args.batch_size, shuffle=False,
+                               device=args.device, drop_last=False)
+
+    return train_batches, test_batches, A, overall_tbatches
+
+
+def split_equal_noniid(train_dataset, test_dataset, shards, edge_frac, clients):
+    """
+    :param train_dataset:
+    :param test_dataset:
+    :param shards:
+    :param edge_frac:
+    :param clients:
+    :return:
+    """
+    total_shards = shards * clients
+    shard_size = int(len(train_dataset.data) / total_shards)
+    idx_shard = [i for i in range(total_shards)]
+    train_dict_users = {i: np.array([]) for i in range(clients)}
+    idxs = np.arange(total_shards * shard_size)
+    labels = train_dataset.targets
+    dict_label_dist = {i: np.array([]) for i in range(clients)}
+
+    # sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+    idxs = idxs_labels[0, :]
+    label_count = np.bincount(idxs_labels[1])  # 统计标签数组中各数字（从小到大顺序）出现的个数
+
+    # generate adj
+    A = np.zeros((clients, clients))
+    num_label = len(set(labels))
+    label_dist = [[] for _ in range(num_label)]
+
+    # partitions for train data
+    for i in range(clients):
+        rand_set = np.random.choice(idx_shard, shards, replace=False)
+        idx_shard = list(set(idx_shard) - set(rand_set))
+        selected_labels = idxs_labels[1, rand_set * shard_size]
+        label_type = np.array(list(set(selected_labels)))
+        sample_size = [np.count_nonzero(selected_labels == j) for j in label_type]   # 统计列表中非零元素的个数作为采样的大小
+        int(shard_size * shards / len(label_type))
+        dict_label_dist[i] = np.array((label_type, sample_size))
+
+        for j, l in enumerate(label_type):  # 遍历label_type的索引值和元素值
+            start_idx = sum(label_count[0:l])
+            end_idx = start_idx + label_count[l]
+            sample_array = idxs[start_idx: end_idx]
+            train_dict_users[i] = np.concatenate(
+                (train_dict_users[i], np.random.choice(
+                    sample_array, sample_size[j] * shard_size, replace=False)), axis=0)
+
+        # for cifar-100, control the sparsity of A
+        label_size = np.array([np.count_nonzero(
+            labels[train_dict_users[i].astype(int)] == j) for j in label_type])
+        pram_label_idx = np.array(sorted(range(len(label_size)),
+                key=lambda i: label_size[i])[min(-train_dataset.topk, shards):])
+
+        for label_type in label_type[pram_label_idx]:
+            label_dist[label_type].append(i)
+
+    # prepare A
+    link_list = []
+    for user_arr in label_dist:
+        for user_a in user_arr:
+            for user_b in user_arr:
+                link_list.append([user_a, user_b])
+
+    link_sample = list(range(len(link_list)))
+    link_idx = np.random.choice(link_sample, int(edge_frac * len(link_list)), replace=False)
+    for idx in link_idx:
+        # A[link_list[idx][0], link_list[idx][1]] = A[link_list[idx][0], link_list[idx][1]] + 1
+        A[link_list[idx][0], link_list[idx][1]] = 1
+
+    # partition for test data
+    total_shards = shards * clients
+    shard_size = int(len(test_dataset.data) / total_shards)
+    test_dict_users = {i: np.array([]) for i in range(clients)}
+    idxs = np.arange(total_shards * shard_size)
+    labels = test_dataset.targets
+
+    # sort labels
+    idxs_labels = np.vstack((idxs, labels))
+    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
+    idxs = idxs_labels[0, :]
+    label_count = np.bincount(idxs_labels[1])
+
+    for i in range(clients):
+        for j, l in enumerate(dict_label_dist[i][0]):
+            start_idx = sum(label_count[0:l])
+            end_idx = start_idx + label_count[l]
+            sample_array = idxs[start_idx: end_idx]
+            test_dict_users[i] = np.concatenate(
+                (test_dict_users[i], np.random.choice(
+                    sample_array, dict_label_dist[i][1][j] * shard_size, replace=False)), axis=0)
+
+    return train_dict_users, test_dict_users, torch.tensor(normalize_adj(A), dtype=torch.float32)
+
+
+class Batches():
+    def __init__(self, dataset, batch_size, shuffle, device, set_random_choices=False, num_workers=0, drop_last=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.set_random_choices = set_random_choices
+        self.device = device
+        self.dataloader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, num_workers=num_workers, pin_memory=True, shuffle=shuffle,
+            drop_last=drop_last
+        )
+
+    def __iter__(self):
+        if self.set_random_choices:
+            self.dataset.set_random_choices()
+        if self.device is not None:
+            return ({'input': x.to(self.device), 'target': y.to(self.device).long()} for (x, y) in self.dataloader)
+        else:
+            return ({'input': x, 'target': y.long()} for (x, y) in self.dataloader)
+
+    def __len__(self):
+        return len(self.dataloader)
+
+
+#####################
+## data augmentation
+#####################
+class Crop(namedtuple('Crop', ('h', 'w'))):
+    def __call__(self, x, x0, y0):
+        return x[:, y0:y0 + self.h, x0:x0 + self.w]
+
+    def options(self, x_shape):
+        C, H, W = x_shape
+        return {'x0': range(W + 1 - self.w), 'y0': range(H + 1 - self.h)}
+
+    def output_shape(self, x_shape):
+        C, H, W = x_shape
+        return (C, self.h, self.w)
+
+
+class FlipLR(namedtuple('FlipLR', ())):
+    def __call__(self, x, choice):
+        return x[:, :, ::-1].copy() if choice else x
+
+    def options(self, x_shape):
+        return {'choice': [True, False]}
+
+
+class Cutout(namedtuple('Cutout', ('h', 'w'))):
+    def __call__(self, x, x0, y0):
+        x = x.copy()
+        x[:, y0:y0 + self.h, x0:x0 + self.w].fill(0.0)
+        return x
+
+    def options(self, x_shape):
+        C, H, W = x_shape
+        return {'x0': range(W + 1 - self.w), 'y0': range(H + 1 - self.h)}
+
+
+class Transform:
+    def __init__(self, dataset, transforms):
+        self.dataset, self.transforms = dataset, transforms
+        self.choices = None
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        data, labels = self.dataset[index]
+        for choices, f in zip(self.choices, self.transforms):
+            args = {k: v[index] for (k, v) in choices.items()}
+            data = f(data, **args)
+        return data, labels
+
+    def set_random_choices(self):
+        self.choices = []
+        x_shape = self.dataset[0][0].shape
+        N = len(self)
+        for t in self.transforms:
+            options = t.options(x_shape)
+            x_shape = t.output_shape(x_shape) if hasattr(t, 'output_shape') else x_shape
+            self.choices.append({k: np.random.choice(v, size=N) for (k, v) in options.items()})
+
+
+def normalise(x, mean, std):    # 正态化函数
+    x, mean, std = [np.array(a, np.float32) for a in (x, mean, std)]
+    x -= mean * 255
+    x *= 1.0 / (255 * std)
+    return x
+
+
+def pad(x, border=4):  # 矩阵填充函数
+    return np.pad(x, [(0, 0), (border, border), (border, border), (0, 0)], mode='reflect')
+
+
+def transpose(x, source='NHWC', target='NCHW'):   # 矩阵转置
+    return x.transpose([source.index(d) for d in target])
+
+
+def normalize_adj(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv = np.power(rowsum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0.
+    r_mat_inv = sp.diags(r_inv)
+    mx = r_mat_inv.dot(mx)
+    return mx
